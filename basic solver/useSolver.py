eeb8a4bbbf699ddbec9d61649f6fc92f4da85ac8
MineLT <58398020+MineLT@users.noreply.github.com>
2020-07-09 15:47:19 +0200

    Add files via upload

--- basic solver/useSolver.py
+++ basic solver/useSolver.py
@@ -9,11 +9,6 @@ cube = [0,0,0,0,0,0,0,0,0,
          2,2,2,2,2,2,2,2,2,
          5,5,5,5,5,5,5,5,5]
 
-mel = SolveBH.randomCube(cube)
-print(mel)
-outils.printCube(cube)
-
-#cube = ast.literal_eval(input('Representation of your cube : '))
+cube = ast.literal_eval(input('Representation of your cube : '))
 convertRes, res = SolveBH.solve(cube)
 print(res)
-
